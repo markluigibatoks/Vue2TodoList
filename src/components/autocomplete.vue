367e52943719a67705c5643b8661238327efd692
MARK LUIGI BATOCTOY <markluigibatoks@gmail.com>
2022-06-10 02:11:43 -0700

    feat: autocomplete with chips

--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -2,36 +2,35 @@
   <div
     class="autocomplete"
     tabindex="0"
-    @blur="showList = false"
-    @click="$event.target.focus()"
+    @click="focusInput()"
     @focus="showList = true"
+    @blur="showList = false"
   >
-    <label for="input__text">Select</label>
+    <label for="input_text">Select</label>
     <div class="selections">
-      <span class="chip">
-        Chip 1
+      <span
+        v-for="(item, index) in selectedItems"
+        :key="index"
+        class="chip"
+      >
+        {{item}}
         <div
           class="close-button"
-          @click="remove({})"
+          @click="removeSelectedItem(item)"
         >
-          <i class="fa-regular fa-circle-xmark"></i>
-        </div>
-      </span>
-      <span class="chip">
-        Chip 2
-        <div
-          class="close-button"
-          @click="remove({})"
-        >
-          <i class="fa-regular fa-circle-xmark"></i>
+          <i class="fa-solid fa-circle-xmark"></i>
         </div>
       </span>
 
       <input
+        id="input_text"
+        ref="input"
+        v-model="query"
         type="text"
         autocomplete="off"
         @focus="showList = true"
         @blur="showList = false"
+        @keydown.delete="handleKeydownDelete"
       >
     </div>
 
@@ -40,16 +39,21 @@
       class="list"
     >
       <li
+        v-for="(item, index) in getFilteredResults"
+        :key="index"
         class="list__item"
-        @click="itemClick()"
+        @click="selectItem(item)"
       >
-        Chip 3
+        {{ item }}
       </li>
-      <li>Chip 4</li>
-      <li>Chip 5</li>
-      <li>Chip 6</li>
-      <li>Chip 7</li>
     </ul>
+
+    <div
+      class="dropdown-button"
+      :class="{ 'show-list': showList }"
+    >
+      <i class="fa-solid fa-caret-down"></i>
+    </div>
   </div>
 </template>
 
@@ -57,86 +61,159 @@
 export default {
   name: 'AutoComplete',
 
+  props: {
+    items: {
+      type: Array,
+      // required: true,
+      default () {
+        return [
+          'Chip 1', 'Chip 2', 'Chip 3', 'Chip 4', 'Chip 5'
+        ]
+      }
+    }
+  },
+
   data () {
     return {
-      showList: false
+      clonedItems: [],
+      showList: false,
+      query: '',
+      result: [],
+      selectedItems: [],
+      prevQuery: ''
+    }
+  },
+
+  computed: {
+    getFilteredResults () {
+      return this.result.filter(item => item.toLowerCase().includes(this.query.trim().toLowerCase())).sort()
+    }
+  },
+
+  created () {
+    if (this.items !== undefined) {
+      this.clonedItems = [...this.items]
+      this.result = [...this.items]
     }
   },
 
   methods: {
-    remove (item) {
-      const index = this.friends.indexOf(item.name)
-      if (index >= 0) this.friends.splice(index, 1)
+    focusInput () {
+      this.$refs.input.focus()
     },
 
-    itemClick () {
-      console.log('click registered')
+    removeSelectedItem (item) {
+      this.spliceArray(item, this.selectedItems)
+      this.result.push(item)
+    },
+
+    spliceArray (item, array) {
+      const idx = array.indexOf(item)
+      array.splice(idx, 1)
+    },
+
+    selectItem (item) {
+      this.spliceArray(item, this.result)
+      this.selectedItems.push(item)
+    },
+
+    handleKeydownDelete () {
+      console.log(this.query, this.query.length > 0)
+      if (this.selectedItems.length > 0 && !this.query.length > 0) {
+        this.result.push(this.selectedItems.pop())
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.autocomplete {
+  width: 100%;
+  background: skyblue;
+  cursor: text;
+  position: relative;
+  padding: 8px;
+  border-radius: 8px;
+
+  .dropdown-button {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+    transition: 0.5s;
   }
 
-  .autocomplete{
-    width: 100%;
-    background: skyblue;
-    cursor: pointer;
+  .dropdown-button.show-list {
+    transform: rotate(180deg);
+  }
 
-    label {
-      font-size: 16px;
-      opacity: 0.7;
-    }
+  label {
+    font-size: 16px;
+    opacity: 0.7;
+    margin-bottom: 8px;
+    display: block;
+  }
 
-    .selections {
-      span.chip {
-        border-radius: 30px;
-        background: skyblue;
-        opacity: 0.7;
-        border: 1px solid #000;
-        padding: 4px 15px;
+  .selections {
+    position: relative;
 
-        .close-button{
-          display: inline;
-          margin-left: 10px;
-        }
+    span.chip {
+      border-radius: 30px;
+      background: #555;
+      padding: 4px 36px 4px 16px;
+      color: #fff;
+      display: inline-block;
+      margin-right: 8px;
+      position: relative;
 
-        .close-button:hover{
-          color: #000;
-        }
-      }
-
-      input {
-        outline: none;
-        border: none;
-      }
-    }
-
-    .list{
-      list-style: none;
-      background: #fff;
-
-      li{
-        padding: 15px 10px;
+      .close-button {
+        display: block;
         cursor: pointer;
+        position: absolute;
+        top: 50%;
+        right: 8px;
+        transform: translateY(-50%);
       }
 
-      li:nth-child(odd){
-        background: #fff;
-      }
-
-      li:nth-child(even){
-        background: #ddd;
-      }
-
-      li:hover{
-        background: skyblue;
+      .close-button:hover {
+        color: rgb(218, 218, 218);
       }
     }
+
+    input {
+      outline: none;
+      border: none;
+      background: none;
+    }
   }
+
+  .list {
+    list-style: none;
+    background: #fff;
+    border-bottom: 1px solid #ddd;
+
+    li {
+      padding: 15px 10px;
+      cursor: pointer;
+    }
+
+    li:nth-child(odd) {
+      background: #fff;
+    }
+
+    li:nth-child(even) {
+      background: #ddd;
+    }
+
+    li:hover {
+      background: skyblue;
+    }
+  }
+}
 </style>
